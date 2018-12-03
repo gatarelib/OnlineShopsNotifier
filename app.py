113499b0d3331215cc8daee36f3206f4c862503b
Gatare Libère <gatarelib@gmail.com>
2018-12-03 13:40:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+__author__='libere'
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -5,6 +7,8 @@ request = requests.get("https://bit.ly/2E8LGBM")
 content = request.content
 soup = BeautifulSoup(content, "html.parser")
 element = soup.find("span", {"dir": "ltr", "data-price": "173999"})
-print(element.text.strip())
+string_price = element.text.strip() #"Price"
+
+
 # strip() method removes all trailing spaces
 #<span dir="ltr" data-price="173999">173,999</span>
